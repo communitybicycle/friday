f432d3c5f78c8be7cb4b31401b4770cd9c9a7141
Hal Shin <hal.shin@alumni.ubc.ca>
2021-01-01 20:45:01 -0800

    Add user reducer

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import dataReducer from "./dataReducer";
 import metaReducer from "./metaReducer";
+import userReducer from "./userReducer";
 
 export const rootReducer = combineReducers({
   data: dataReducer,
   meta: metaReducer,
+  user: userReducer,
 });
