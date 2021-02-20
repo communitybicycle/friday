5ec85dff2202aa620678617afad676ba7b6f4414
Hal Shin <hal.shin@alumni.ubc.ca>
2021-02-20 12:52:49 -0800

    Add filter to actions

--- src/pages/actions/Actions.tsx
+++ src/pages/actions/Actions.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import { Flex, Input } from "@chakra-ui/core";
+import ActionCard from "components/ActionCard";
+import PageHeader from "components/PageHeader";
+import React, { useMemo, useState } from "react";
 import { hot } from "react-hot-loader";
-import PageHeader from "../../components/PageHeader";
-import { Flex } from "@chakra-ui/core";
-import { useHistory } from "react-router";
 import { useSelector } from "react-redux";
-import { RootState } from "../../reducers/store";
-import ActionCard from "../../components/ActionCard";
+import { useHistory } from "react-router";
+import { RootState } from "reducers/store";
 
 const Actions: React.FC = () => {
   const history = useHistory();
   const { actions } = useSelector((state: RootState) => state.data);
+  const [search, setSearch] = useState("");
+
+  const filteredActions = useMemo(() => {
+    return Object.values(actions).filter((action) => {
+      const q = search.toLowerCase();
+
+      const name = action.name.toLowerCase();
+      const desc = action.description ? action.description.toLowerCase() : "";
+
+      return name.includes(q) || desc.includes(q);
+    });
+  }, [search]);
 
   return (
     <div>
@@ -20,8 +32,16 @@ const Actions: React.FC = () => {
         buttonText="Add New"
         isDisabled
       />
+      <Input
+        value={search}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setSearch(e.target.value)
+        }
+        placeholder="Search actions..."
+        mb={4}
+      />
       <Flex flexWrap="wrap">
-        {Object.values(actions)
+        {filteredActions
           .sort((a, b) => (a.name && b.name && a.name > b.name ? 1 : -1))
           .map((action) => (
             <ActionCard action={action} key={action.id} />
