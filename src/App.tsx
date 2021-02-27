02f028025327e7d0958fe21a5279574a5b19327d
Hal Shin <hal.shin@alumni.ubc.ca>
2021-02-27 01:17:35 -0800

    Proper dark mode init

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,25 @@ import { Box, Flex, useColorMode } from "@chakra-ui/core";
 import ElectronStoreSynchronizer from "components/ElectronStoreSynchronizer";
 import Navbar from "components/layout/Navbar";
 import { BG_COLOR, NAVBAR_WIDTH } from "data/constants";
-import React from "react";
+import React, { useEffect } from "react";
 import { Scrollbars } from "react-custom-scrollbars";
 import { hot } from "react-hot-loader";
 import { useSelector } from "react-redux";
 import { RootState } from "reducers/store";
+import { eStore } from "utils/eStore";
 import Router from "./Router";
 
 const App: React.FC = () => {
-  const { colorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   const { isMenuOpen } = useSelector((state: RootState) => state.meta);
+
+  useEffect(() => {
+    const savedColorMode = eStore.get("colorMode");
+
+    if (savedColorMode !== colorMode) {
+      toggleColorMode();
+    }
+  }, []);
 
   return (
     <Flex h="100%" bg={BG_COLOR[colorMode]}>
